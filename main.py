6378216ab6789bffbebdb88759efa1a177bf77c8
jorge <jresino@gmail.com>
2025-02-18 18:38:28 +0100

    docker compose

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+import pathlib
+from pydantic import BaseModel
+
+
+class requestModel(BaseModel):
+    text: str
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def hello_world():
-    return {'response': 'Hello World'}
+@app.post('/')
+def hello_world(request: requestModel):
+
+    path = pathlib.Path(__file__).parent.absolute()
+    file_path = path / 'data' / 'data.txt'
+    print(file_path)
+    print(request.text)
+    with open(file_path, 'w') as f:
+        f.write(request.text)
+    return {'response': f'Text : {request.text}'}
